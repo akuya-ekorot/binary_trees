1e1837f9b0c8b85f3abaf632d247cba502325862
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-30 09:27:59 +0300

    create a binary tree node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+  binary_tree_t *node;
+
+  node = malloc(sizeof(binary_tree_t));
+
+  if (!node)
+    return (NULL);
+
+  node->n = value;
+  node->parent = parent;
+  node->left = NULL;
+  node->right = NULL;
+  
+  return (node);
+}
