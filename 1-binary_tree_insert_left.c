1f666c68d1e808148a232821507da7ecf32fd204
Emmanuel Munubi Mwangi <84630188+Emmanuel-Munubi@users.noreply.github.com>
2023-05-30 16:37:33 +0300

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,19 +10,16 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+    binary_tree_t *left;
 
     if (!parent)
         return (NULL);
     
-    new_node = malloc(sizeof(binary_tree_t));
-    if (!new_node)
-        return (NULL);
-    
-    new_node->n = value;
-    new_node->parent = parent;
-    new_node->left = NULL;
-    new_node->right = NULL;
+    left = binary_tree_node(parent, value);
+	if (!left)
+		return (NULL);
+
+	left->n = value;
 
     if (!parent->left)
     {
@@ -30,10 +27,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
     }
     else
     {
-        new_node->left = parent->left;
-        parent->left = new_node;
-        new_node->left->parent = new_node;
+        left->left = parent->left;
+        parent->left = left;
+        left->left->parent = left;
     }
 
-    return (new_node);
+    return (left);
 }
