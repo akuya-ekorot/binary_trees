dbd92087def243091fc0bfc3064d8b43892dbda9
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-30 11:56:51 +0300

    insert node to right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inserts node to the right of a parent node
+ * @parent: the parent node
+ * @value: the value for the n member of the node
+ *
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right;
+
+	if (!parent)
+		return NULL;
+
+	right = binary_tree_node(parent, value);
+	if (!right)
+		return (NULL);
+
+	right->n = value;
+	right->left = NULL;
+
+	if (parent->right)
+	{
+		right->right = parent->right;
+		right->right->parent = right;
+		parent->right = right;
+	}
+	else
+	{
+		parent->right = right;
+	}
+
+	return (right);
+}
