86f81172e5831675f38576e65884f653b893af4f
Akuya Ekorot <akuyaekorot@gmail.com>
2023-05-31 12:13:38 +0300

    add file

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 
 #endif /* _MAIN_H_ */
