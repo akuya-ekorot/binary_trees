da3c0d901ca2463aec91e6ceebbecf62a734fa40
Emmanuel-Munubi <manumunubi@gmail.com>
2023-06-01 13:04:43 +0300

    returns a pointer to the sibling

--- binary_trees.h
+++ binary_trees.h
@@ -45,5 +45,6 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 #endif /* _MAIN_H_ */
